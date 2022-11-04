c9064e5c5bc93e3948aacf9d97a22cd78c85493b
emilymsq <emilymosq4@gmail.com>
2022-11-04 10:35:28 +0100

    enviar email

---
+++ src/Controller/MailController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MailController extends AbstractController
+
+{
+    /**
+    * @Route("/email")
+    */
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('info@coderf5.es')
+            ->to('emilymosq4@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Conseguido!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        dump('Enviado');die;
+
+    }
+}
