094392429b6ae469a533f8966bf26cd9258de398
Ana Aragon <ana.aragon88@gmail.com>
2022-11-24 12:23:55 +0100

    subir a dev

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,18 +16,13 @@ class SecurityController extends AbstractController
         //     return $this->redirectToRoute('target_path');
         // }
 
-        return $this->json('Created new user successfully with id ');
-
         // get the login error if there is one
-        /*
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-*/
     }
-
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
